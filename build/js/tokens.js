d4fbfe30c6c2234389e8e7cd3f7156720f662193
unknown <diogosilvaoficial@gmail.com>
2023-02-15 22:23:35 -0300

    Initial Commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 16 Feb 2023 01:19:15 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#27272a",
+  "ColorGray900": "#18181b",
+  "ColorOrange500": "#ea580c"
+};
