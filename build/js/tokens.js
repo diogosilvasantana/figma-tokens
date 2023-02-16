af0e372483ee2f9729326576d1578968cb31608d
diogosilvasantana <diogosilvasantana@users.noreply.github.com>
2023-02-16 02:10:14 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,9 +1,12 @@
 /**
  * Do not edit directly
- * Generated on Thu, 16 Feb 2023 02:03:09 GMT
+ * Generated on Thu, 16 Feb 2023 02:10:13 GMT
  */
 
 module.exports = {
+  "Sizes2": 8,
+  "Sizes4": 16,
+  "Sizes8": 32,
   "ColorWhite": "#ffffff",
   "ColorGray200": "#c1c1c4",
   "ColorGray500": "#50505c",
